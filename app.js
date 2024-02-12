30bba8341cd1b3c82c1b25fa0558f4980d84d597
Burhan Nola <burhan.nola@gmail.com>
2024-02-12 13:42:09 +0700

    update variables

--- app.js
+++ app.js
@@ -20,31 +20,10 @@ const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
-// Define a route for the root URL '/'
-// app.get("/", (req, res) => {
-//   res.send("Hello"); // Send a response when the root URL is accessed
-// });
-
-// // Define an endpoint to create a new assistant
-// app.post("/createAssistant", async (req, res) => {
-//   // Use the OpenAI API to create a new assistant
-//   const assistant = await openai.beta.assistants.create({
-//     name: "Nola Assitant for Information", // Name of the assistant
-//     description:
-//       "You are the person who provide any information about Nola. You can give information from the Instagram account @nola.school", // Description of the assistant
-//     model: "gpt-3.5-turbo-0125", // The model used by the assistant
-//     instructions:
-//       "You are the person who provide any information about Nola. You can give information from the Instagram account @nola.school", // Detailed instructions for the assistant
-//     tools: [], // Additional tools for the assistant (if any)
-//   });
-
-//   res.send(assistant); // Send the created assistant object as a response
-// });
-
 // Add an endpoint to run the assistant
 app.post("/chatbot", async (req, res) => {
   let body = req.body; // Get the request body
-  let sThread = "";
+  let sThread = body.sThread;
   sAssistant = process.env["sAssistant"];
 
   // Check if it's a new conversation or an existing thread
@@ -69,9 +48,11 @@ app.post("/chatbot", async (req, res) => {
 
   // Retrieve messages from the thread
   const threadMessages = await openai.beta.threads.messages.list(sThread);
+  const pertanyaan = req.body.prompt;
   const jawaban = threadMessages.body.data[0].content[0].text.value;
   // Send the thread messages and thread ID as a response
   res.send({
+    question: pertanyaan,
     message: jawaban.split("【")[0].replace(/\n/g, "<br>"),
     threadID: sThread,
   });
@@ -94,7 +75,6 @@ async function waitForRunComplete(sThreadId, sRunId) {
   }
 }
 
-// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Service start on port ${port}`);
 });
